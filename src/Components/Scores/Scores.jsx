e039b9483b7c3200b1f245ceac9d765fbca26068
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-24 13:04:47 -0600

    refactor: Scores.jsx code started.

---
+++ src/Components/Scores/Scores.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Jumbotron, Button, Link } from 'react-bootstrap';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            logged_in: false
+        }
+    }
+
+    loginButtonHandler = () => {
+        this.setState({ logged_in: !this.state.logged_in });
+    }
+
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>Scores and Results Analysis</h1>
+                    <hr className="my-2"></hr>
+                    <p>
+                        Below is a representation of your math game results.
+                    </p>
+                    {
+                        (this.state.logged_in) ?
+                            (
+                                <div>
+                                    <Button id="logout" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Logout</Button>
+                                </div>
+                            ) :
+                            (
+                                <div>
+                                    <Button id="login" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Login</Button>
+                                    <a className="btn btn-dark" id="signup" href="#">Signup Page</a>
+                                </div>
+                            )
+                    }
+                </Jumbotron>
+            </div>
+        );
+    }
+}
