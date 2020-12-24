3d3000ec5066a2464f3abd72bcd6cefad796b6bd
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-24 12:38:15 -0600

    feat: Homepage component started.
    Added initial code for homepage using react component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container, Navbar} from 'react-bootstrap';
+import Home from './Components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+              <a className="navbar-brand" href="/">Home</a>
+              <a className="navbar-brand" href="/game">Game</a>
+              <a className="navbar-brand" href="/scores">Scores</a>
+              <a className="navbar-brand" href="/admin">Admin</a>
+            </div>
+          </Navbar>
+          <Home {...this.state} />
+
+        </Container>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Jumbotron, Button, Link} from 'react-bootstrap';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            logged_in: false
+        }
+    }
+
+    loginButtonHandler = () => {
+        // let tempState = this.state;
+        // tempState.logged_in = !this.state.logged_in;
+        this.setState({logged_in: !this.state.logged_in});
+    }
+
+    render() {
+        return(
+            <div>
+                <Jumbotron>
+                    <h1>Welcome!</h1>
+                    <hr className="my-2"></hr>
+                    <p>
+                        Welcome to the homepage.  If you haven't already signed up for an account,
+                        please do so.  If you are not already logged in, please login to gain full
+                        access to the site.
+
+                        If you want to test the game without creating a username and password here
+                        are some test credentials:
+                    </p>
+                    <p>Username: userTest</p>
+                    <p>Password: testPassword3456</p>
+
+                    {
+                        (this.state.logged_in) ?
+                        (
+                            <div>
+                                <Button id="logout" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Logout</Button>
+                            </div>
+                        ) :
+                        (
+                            <div>
+                                <Button id="login" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Login</Button>
+                                <a className="btn btn-dark" id="signup" href="#">Signup Page</a>
+                            </div>
+                        )
+                    }
+
+                </Jumbotron>
+            </div>
+        );
+    }
+}
