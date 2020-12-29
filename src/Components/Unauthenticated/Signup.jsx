2eda080266748de421bb5a2a9a67d626922d21d2
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-29 12:17:27 -0600

    feat: Signup page username, password, and passwordConfirmation check fields added.

--- src/Components/Unauthenticated/Signup.jsx
+++ src/Components/Unauthenticated/Signup.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Jumbotron, Button, } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
 
 export default class Signup extends React.Component {
     constructor(props) {
@@ -9,16 +11,63 @@ export default class Signup extends React.Component {
             username: "",
             password: "",
             passwordConfirmation: "",
+            passwordsMatch: false
         }
+
+        //this.inputFieldHandler = this.inputFieldHandler.bind(this);
+    }
+
+    inputFieldHandler = (event) => {
+        let tempState = this.state;
+        tempState[event.target.name] = event.target.value;
+
+        if (event.target.name.includes('password')) {
+            this.passwordMatchCheck();
+        }
+
+        this.setState(tempState);
+    }
+
+    passwordMatchCheck = () => {
+        let tempState = this.state;
+
+        if (this.state.password === this.state.passwordConfirmation) {
+            tempState.passwordsMatch = true;
+            this.setState(tempState);
+        } else {
+            tempState.passwordsMatch = false;
+            this.setState(tempState);
+        }
+    }
+
+    submitButtonHandler() {
+        axios.post('/accounts/signup/', {
+            username
+        })
     }
 
     render() {
         return (
             <div>
-                <form className="site-form" action="" method='POST'>
-                    <input type="hidden" name="next" value="" />
-                    <button type="submit" className="btn btn-dark">Signup</button>
-                </form>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>User Name</Form.Label>
+                        <Form.Control name="username" type="input" placeholder="Enter a username" onChange={this.inputFieldHandler} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control name="password" type="password" placeholder="Password" onChange={this.inputFieldHandler} isValid={this.state.passwordsMatch} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPasswordConfirmation">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control name="passwordConfirmation" type="password" placeholder="Password confirmation" onChange={this.inputFieldHandler} isValid={this.state.passwordsMatch} />
+                        <Form.Text className="text-muted">Passwords don't match</Form.Text>
+                    </Form.Group>
+
+                    <Button className="btn btn-dark" type="submit">Submit</Button>
+                </Form>
             </div>
         )
     }
