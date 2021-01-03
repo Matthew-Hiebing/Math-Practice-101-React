13d79e465ec2aeeec26eea723abf5ad733d7b6d2
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-03 14:09:12 -0600

    feat: Checkbox splash screen functionality complte.

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -10,41 +10,41 @@ export default class Game extends React.Component {
             game_properties: {
                 splash_screen: {
                     splash_screen_text: null,
-                    splash_screen_preference: false
+                    splash_screen_preference: false,
+                    checkbox_state: false
                 },
 
             }
         }
     }
 
+    componentDidMount() {
+        axiosInstance.get('/api/game-properties/math')
+        .then((response) => {
+            let tempState = this.state;
+            tempState.game_properties.splash_screen.splash_screen_text = response.data.splash_screen.splash_screen_text;
+            tempState.game_properties.splash_screen.splash_screen_preference = response.data.splash_screen.splash_screen_preference;
+
+            this.setState(tempState);
+        });
+    }
+
     loginButtonHandler = () => {
         this.setState({ logged_in: !this.state.logged_in });
     }
 
-    componentDidMount() {
-        axiosInstance.get('/api/game-properties/math')
-        .then((response) => {
-             this.setState({ game_properties: response.data });
-        });
-    }
-
     splashScreenHandler = (event) => {
+        let tempState = this.state;
+        tempState.game_properties.splash_screen.checkbox_state = event.target.checked;
+
+        this.setState(tempState);
+
         axiosInstance.post('api/user_preferences/set_preference', {
             splash_screen_name: "Math",
-            display_on_refresh: !event.target.value
+            display_on_refresh: !this.state.game_properties.splash_screen.checkbox_state
         })
         .then((response) => {
-            if (response.status === 200) {
-                this.setState({
-                    game_properties: {
-                        splash_screen: {
-                            splash_screen_preference: this.state.game_properties.splash_screen.splash_screen_preference
-                        }
-                    }
-                })
-            } else {
-                console.log(response.data)
-            }
+            console.log(response.data);
         })
         .catch((error) => {
             console.log(error);
@@ -63,22 +63,21 @@ export default class Game extends React.Component {
                         <div className= "alert alert-primary">
                             <p>{this.state.game_properties.splash_screen.splash_screen_text}</p>
                             <p>
-                                <input name="splash" type="checkbox" value={this.state.game_properties.splash_screen.splash_screen_preference} onChange={this.splashScreenHandler} /> Don't show this message again.
+                                <input name="splash" type="checkbox" checked={this.state.game_properties.splash_screen.checkbox_state} onChange={this.splashScreenHandler} /> Don't show this message again.
                             </p>
                         </div>
                     ) : (null)
                     }
                     {
-                        (this.state.logged_in) ?
+                        (this.props.is_logged_in) ? // if logged in show logout button.
                             (
                                 <div>
-                                    <Button id="logout" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Logout</Button>
+                                    <Button id="logout" type='submit' className="btn btn-dark" onClick={() => this.props.history.push('/logged-out')}>Logout</Button>
                                 </div>
-                            ) :
+                            ) : // if logged in show login button.
                             (
                                 <div>
                                     <Button id="login" type='submit' className="btn btn-dark" onClick={() => this.props.history.push('/login')}>Login</Button>
-                                    <Button id="signup" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Signup</Button>
                                 </div>
                             )
                     }
--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/',
     timeout: 5000,
     headers: {
-        'Authorization': "Bearer" + localStorage.getItem('access_token'),
+        'Authorization': "Bearer " + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
         'accept': 'application/json'
     }
