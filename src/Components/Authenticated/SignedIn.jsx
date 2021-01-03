6b5782b5966bb8833d6efe57c916ad2e85a8a83e
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-03 16:49:30 -0600

    refactor: Changed signin page message.

--- src/Components/Authenticated/SignedIn.jsx
+++ src/Components/Authenticated/SignedIn.jsx
@@ -13,7 +13,7 @@ export default class SignedIn extends React.Component {
                 <Jumbotron>
                     <h1>Signed In</h1>
                     <hr className="my-2"></hr>
-                    <p>You successfully created an account.  Please login to begin playing the game.</p>
+                    <p>You successfully created an account and have automatically been logged in.  Head over to the game page to start playing.</p>
                     <Button id="login" type='submit' className="btn btn-dark" onClick={this.loginButtonHandler}>Login</Button>
                 </Jumbotron>
             </div>
