97dac8050d0130bd3570537f3f4d39faf2bac759
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-04 13:35:17 -0600

    feat: All game page functionality working besides Axios request.

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 import { Jumbotron, Button, Form } from 'react-bootstrap';
 import axiosInstance from '../../helpers/axiosInstance';
+import {multiplication, division, randomFunc, randomProblemGenerator} from './MathProblemGenerator';
 
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
+
+        // this.userInput = React.createRef();
 
         this.state = {
             game_properties: {
@@ -12,6 +16,12 @@ export default class Game extends React.Component {
                     splash_screen_text: null,
                     splash_screen_preference: false,
                     checkbox_state: false
+                },
+                problem: {
+                    problem_string: "Press START to begin",
+                    problem_answer: null,
+                    user_input: "",
+                    status: "not_answered"
                 }
             }
         }
@@ -50,17 +60,74 @@ export default class Game extends React.Component {
         });
     }
 
-    submitHandler(event) {
-        // Prevent enter key from refreshing page.
+    submitHandler = (event) => {
+        // Prevent form from sending.
         event.preventDefault();
     }
 
-    startButtonHandler(event) {
+    startButtonHandler = (event) => {
         // Show random math problem.
+        let problem = randomProblemGenerator();
+        console.log(problem);
+        let tempState = this.state;
+        tempState.game_properties.problem.status = "not_answered"
+        tempState.game_properties.problem = { ...tempState.game_properties.problem, ...problem }
+        tempState.game_properties.problem.user_input = ""
+        this.userInput.focus();
+
+        this.setState(tempState);
     }
 
-    checkButtonHandler(event) {
+    checkButtonHandler = (event) => {
         // Check if user's answer is corrrect.
+        let input = parseInt(this.state.game_properties.problem.user_input);
+        let answer = parseInt(this.state.game_properties.problem.problem_answer);
+        let tempState = this.state;
+
+        if (input === answer) {
+            console.log("Correct");
+            tempState.game_properties.problem.status = "correct"
+        } else {
+            console.log("Incorrect")
+            tempState.game_properties.problem.status = "incorrect"
+        }
+
+        this.setState(tempState);
+    }
+
+    answerChangeHandler = (event) => {
+        // Grab the user's answer string.
+        let input = event.target.value
+        let tempState = this.state;
+        tempState.game_properties.problem.user_input = input
+
+        this.setState(tempState);
+        console.log(input)
+    }
+
+    enterPressHandler = (event) => {
+        // If the key pressed was enter
+        // Execute the checkHandlerGunction
+        // Else do nothing
+        if ((event.key === 'Enter')) {
+            this.checkButtonHandler()
+        } else {
+
+        }
+    }
+
+    sendMathResult = (event) => {
+        // problem_string, problem_answer, user_input, status
+        axiosInstance.post('/api/scoring/submit_score_details', {
+            "math_problem": this.state.game_properties.problem.problem_string,
+            "true_answer": this.state.game_properties.problem.problem_answer,
+            "user_answer": this.state.game_properties.problem.user_input,
+            "question_status": this.state.game_properties.problem.status
+        });
+        console.log(`Axios sent math_problem: ${this.state.game_properties.problem.problem_string}`);
+        console.log(`Axios sent true_answer: ${this.state.game_properties.problem.problem_answer}`);
+        console.log(`Axios sent user_answer: ${this.state.game_properties.problem.user_input}`);
+        console.log(`Axios sent question_status: ${this.state.game_properties.problem.status}`);
     }
 
     render() {
@@ -86,14 +153,35 @@ export default class Game extends React.Component {
                         (
                             <div>
                                 <Button id="startButton" type="button" className="btn btn-primary btn-lg" onClick={this.startButtonHandler}>Start</Button>
-                                <p id="math_problem">Problem will show here.</p>
-                                <Form onSubmit={this.submitHandler}>
-                                    <Form.Group controlId="exampleForm.ControlInput1" >
-                                        <Form.Control name="input" type="input" placeholder="Enter your answer here" autoComplete="off"/>
-                                    </Form.Group>
-                                </Form>
-                                <Button id="checkButton" type="button" className="btn btn-primary btn-lg" onClick={this.checkButtonHandler}>Check</Button>
-                                <p>Results graph goes here</p>
+                                <p id="math_problem">{this.state.game_properties.problem.problem_string}</p>
+                                <Form.Group controlId="exampleForm.ControlInput1" >
+                                        <Form.Control
+                                            ref={(input) => { this.userInput = input; }}
+                                            name="input"
+                                            type="input"
+                                            placeholder="Enter your answer here"
+                                            autoComplete="off"
+                                            onChange={this.answerChangeHandler}
+                                            onKeyPress={this.enterPressHandler}
+                                            value={this.state.game_properties.problem.user_input}
+                                        />
+                                </Form.Group>
+                                <Button id="checkButton" type="button" className={"btn btn-lg " + (
+                                        (this.state.game_properties.problem.status === "correct") ?
+                                            ("btn-success") :
+                                            (this.state.game_properties.problem.status == "incorrect") ?
+                                                ("btn-danger") :
+                                                ("btn-primary")
+                                )} onClick={this.checkButtonHandler}>
+                                        {
+                                            (this.state.game_properties.problem.status === "correct") ?
+                                                ("Correct") :
+                                                (this.state.game_properties.problem.status == "incorrect") ?
+                                                    ("Incorrect") :
+                                                    ("Check")
+                                        }
+                                </Button>
+                                <p>Graph of results will show here.  Graph data is generated as the user plays the game.</p>
                                 <Button id="logout" type='submit' className="btn btn-dark" onClick={() => this.props.history.push('/logged-out')}>Logout</Button>
                             </div>
                         ) : // if not logged in show login button.
--- src/Components/Game/MathProblemGenerator.js
+++ src/Components/Game/MathProblemGenerator.js
@@ -1,41 +1,38 @@
-const randomFunc = [multiplication, division, addition, subtraction]
+export const randomFunc = [multiplication, division, addition, subtraction]
 
-function randomProblemGenerator() {
-    randomFunc[Math.floor(Math.random() * randomFunc.length)]()
+export function randomProblemGenerator() {
+    return (randomFunc[Math.floor(Math.random() * randomFunc.length)]())
 }
 
 
-function multiplication() {
+export function multiplication() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 * num2;
-    console.log(num1, '*', num2, '=', problemResult);
     let problem =`${num1} x ${num2}`;
-    return problemResult
+    return {"problem_string": problem, "problem_answer": problemResult}
 }
 
 
-function division() {
+export function division() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 12) + 1;
     let problemResult = (num1 * num2) / num2;
-    console.log(num1 * num2, '/', num2, '=', problemResult);
     let problem =`${num1 * num2} ÷ ${num2}`;
-    return problemResult
+    return { "problem_string": problem, "problem_answer": problemResult }
 }
 
 
-function addition() {
+export function addition() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 + num2;
-    console.log(num1, '+', num2, '=', problemResult);
     let problem =`${num1} + ${num2}`;
-    return problemResult
+    return { "problem_string": problem, "problem_answer": problemResult }
 }
 
 
-function subtraction() {
+export function subtraction() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let numList = [num1, num2];
@@ -43,7 +40,6 @@ function subtraction() {
         return a - b
     });
     let problemResult = numList[1] - numList[0];
-    console.log(numList[1], '-', numList[0], '=', problemResult);
     let problem =`${numList[1]} - ${numList[0]}`;
-    return problemResult
+    return { "problem_string": problem, "problem_answer": problemResult }
 }
