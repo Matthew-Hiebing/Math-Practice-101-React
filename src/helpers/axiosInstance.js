248df4e4bb33eb49314c026a284fd441f824d688
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-06 14:25:08 -0600

    perf: Changed access and refresh token refresh times.
    Keeps the user logged in for a specific amount of time.  If their access token expires a new one will automatically be generated unless their refresh token is also expired.

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = 'http://127.0.0.1:8000/'
+
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: baseURL,
     timeout: 5000,
     headers: {
         'Authorization': "Bearer " + localStorage.getItem('access_token'),
@@ -9,5 +11,55 @@ const axiosInstance = axios.create({
         'accept': 'application/json'
     }
 });
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && originalRequest.url === baseURL+'token/refresh/') {
+            localStorage.clear()
+            window.location.href = '/login/';
+            return Promise.reject(error);
+        }
+
+        if (error.response.data.code === "token_not_valid" &&
+            error.response.status === 401 &&
+            error.response.statusText === "Unauthorized")
+            {
+                const refresh_token = localStorage.getItem('refresh_token');
+
+                if (refresh_token) {
+                    const tokenParts = JSON.parse(atob(refresh_token.split('.')[1]));
+
+                    const now = Math.ceil(Date.now() / 1000);
+
+                    if (tokenParts.exp > now) {
+                        return axiosInstance
+                        .post('/token/refresh/', {refresh: refresh_token})
+                        .then((response) => {
+                            localStorage.setItem('access_token', response.data.access);
+                            localStorage.setItem('refresh_token', response.data.refresh);
+
+                            axiosInstance.defaults.headers['Authorization'] = "Bearer " + response.data.access
+                            originalRequest.headers['Authorization'] = "Bearer " + response.data.access;
+
+                            return axiosInstance(originalRequest);
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                    } else {
+                        localStorage.clear()
+                        window.location.href = '/login/';
+                    }
+                } else {
+                    localStorage.clear()
+                    window.location.href = '/login/';
+                }
+            }
+        return Promise.reject(error)
+    }
+);
 
 export default axiosInstance;
