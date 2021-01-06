a9057827b0fb7e02309bff50b3e9bd0af7b98b92
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-06 14:23:08 -0600

    feat: Show user score on scores page.

--- src/Components/Scores/Scores.jsx
+++ src/Components/Scores/Scores.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Jumbotron, Button, } from 'react-bootstrap';
+import axiosInstance from '../../helpers/axiosInstance';
+import ScoresChartTest from './ScoresChartTest';
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            chartData: {
+                correctCounter: 0,
+                incorrectCounter: 0,
+                totalCounter: 0
+            }
         }
+    }
+
+    componentDidMount() {
+        axiosInstance.get('api/scoring/request_score_details')
+        .then((response) => {
+            let tempState = this.state;
+            tempState.chartData.correctCounter = response.data.correct_answer_count;
+            tempState.chartData.incorrectCounter = response.data.incorrect_answer_count;
+            tempState.chartData.totalCounter = response.data.total_questions_answered;
+            this.setState(tempState);
+        });
     }
 
     loginButtonHandler = () => {
@@ -19,19 +37,24 @@ export default class Home extends React.Component {
                 <Jumbotron>
                     <h1>Scores and Results Analysis</h1>
                     <hr className="my-2"></hr>
-                    <p>
-                        Below is a representation of your math game results.
-                    </p>
-                    <p>
-                        Graph of results will show here.  Data for graph is pulled from database.
-                    </p>
+                    <p>Below is a representation of your math game results.</p>
                     {
                         (this.props.is_logged_in) ? // if logged in show logout button.
                             (
                                 <div>
-                                    <Button id="logout" type='submit' className="btn btn-dark" onClick={() => this.props.history.push('/logged-out')}>Logout</Button>
+                                    <div>
+                                        {
+                                            (this.state.chartData.totalCounter > 0) ? (<ScoresChartTest chartData={this.state.chartData} />) : (null)
+                                        }
+                                    </div>
+                                    <Button
+                                    id="logout"
+                                    type='submit'
+                                    className="btn btn-dark"
+                                    onClick={() => this.props.history.push('/logged-out')}>Logout
+                                    </Button>
                                 </div>
-                            ) : // if logged in show login button.
+                            ) : // if logged in show logout button.
                             (
                                 <div>
                                     <Button id="login" type='submit' className="btn btn-dark" onClick={() => this.props.history.push('/login')}>Login</Button>
---
+++ src/Components/Scores/ScoresChart.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+    BarChart, Bar,
+    XAxis,
+    YAxis,
+    Legend,
+} from 'recharts';
+
+export default class ScoresChart extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            axes: [
+                { primary: true, type: 'ordinal', position: 'left' },
+                { position: 'bottom', type: 'linear', stacked: true }
+            ],
+            series: {
+                type: 'bar'
+            },
+            chartData: [
+                {
+                    name: 'Correct', score: 0,
+                },
+                {
+                    name: 'Incorrect', score: 0,
+                },
+                {
+                    name: 'Total', score: 0,
+                },
+            ],
+            chartLayout: {
+                title: 'Math Game Results',
+                yaxis: {
+                    showticklabels: false
+                },
+            }
+        }
+    }
+
+    // componentDidUpdate(prevProps) {
+    //     if (prevProps.chartData.totalCounter !== this.state.chartData[2].score) {
+    //         let tempState = this.state;
+    //         tempState.chartData = [
+    //             {
+    //                 name: 'Correct', score: this.props.correctCounter,
+    //             },
+    //             {
+    //                 name: 'Incorrect', score: this.props.incorrectCounter,
+    //             },
+    //             {
+    //                 name: 'Total', score: this.props.totalCounter,
+    //             },
+    //         ];
+    //         this.setState(tempState);
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                {
+                    (this.state.chartData[2].score > 0) ?
+                        (<BarChart
+                            layout="horizontal"
+                            width={500}
+                            height={300}
+                            data={this.state.chartData.slice()}
+                            margin={{
+                                top: 5, right: 30, left: 20, bottom: 5,
+                            }}
+                        >
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Legend />
+                            <Bar dataKey="score" fill="#82ca9d" />
+                        </BarChart>) : (null)
+                }
+            </div>
+        );
+    }
+}
